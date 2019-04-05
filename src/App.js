b447442c7c164b0858d17a149cdf5c0c96ba435c
MartinLeMintier <martin.le-mintier-de-la-motte-basse@edu.ece.fr>
2019-04-05 10:53:06 +0200

    Question 2

--- src/App.js
+++ src/App.js
@@ -5,18 +5,43 @@ import Artist from './component/Artist';
 import AlbumGrid from './container/AlbumGrid';
 
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
+
+
 
 class App extends Component {
   render() {
     return (
+
+      <Container>
+
+        <Row>
+
+        
+        
       <div>
         <Header />
+
+
+        <Col md ="2">
         <SideMenu />
+
+        </Col>
+
+        <Col md = "10">
         <main>
           <Artist />
           <AlbumGrid />
         </main>
+
+        </Col>
+
+
       </div>
+
+      </Row>
+
+      </Container>
     );
   }
 }
