0cc3177428f7edef6b30384625eaefaa6b16dae9
MartinLeMintier <martin.le-mintier-de-la-motte-basse@edu.ece.fr>
2019-04-05 10:58:43 +0200

    QUestion 4

--- src/container/AlbumGrid.js
+++ src/container/AlbumGrid.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Album from '../component/Album';
+import { Container, Row, Col } from 'reactstrap';
 
 export default class AlbumGrid extends Component {
   render() {
@@ -11,68 +12,105 @@ export default class AlbumGrid extends Component {
             Start Station
           </a>
         </div>
+
+        <Row>
         <section id="albums">
+
+       
+
+        <Col xs = "6" sm ="6" md="3" xl ="2">
           <Album
             image="img/album/album_01.png"
             nom="Native (Deluxe)"
             artist="OneRepublic"
             song={17}
           />
+
+          </Col>
+
+          <Col xs = "6" sm ="6" md="3" xl ="2"></Col>
           <Album
             image="img/album/album_02.png"
             nom="Wax Wings"
             artist="Joshua Radin"
             song={11}
           />
+
+<Col xs = "6" sm ="6" md="3" xl ="2">
           <Album
             image="img/album/album_03.png"
             nom="Wax Modern Vampires of th..."
             artist="Vampire Weekend"
             song={12}
           />
+          </Col>
+
+<Col xs = "6" sm ="6" md="3" xl ="2">
           <Album
             image="img/album/album_04.png"
             nom="Night Visions"
             artist="Imagine Dragons"
             song={11}
           />
+          </Col>
+
+<Col xs = "6" sm ="6" md="3" xl ="2">
           <Album
             image="img/album/album_05.png"
             nom="Trouble Will Find Me"
             artist="The National"
             song={13}
           />
+          </Col>
+
+<Col xs = "6" sm ="6" md="3" xl ="2">
           <Album
             image="img/album/album_06.png"
             nom="Native (Deluxe)"
             artist="OneRepublic"
             song={17}
           />
+          </Col>
+
+<Col xs = "6" sm ="6" md="3" xl ="2">
           <Album
             image="img/album/album_07.png"
             nom="Wax Wings"
             artist="Joshua Radin"
             song={11}
           />
+          </Col>
+
+<Col xs = "6" sm ="6" md="3" xl ="2">
           <Album
             image="img/album/album_08.png"
             nom="Modern Vampires of th..."
             artist="Vampire Weekend"
             song={12}
           />
+          </Col>
+
+<Col xs = "6" sm ="6" md="3" xl ="2">
           <Album
             image="img/album/album_09.png"
             nom="Night Visions"
             artist="Imagine Dragons"
             song={11}
           />
+          </Col>
+
+<Col xs = "6" sm ="6" md="3" xl ="2">
           <Album
             image="img/album/album_10.png"
             nom="Trouble Will Find Me"
             artist="The National"
             song={13}
           />
+          </Col>
+
+
         </section>
+        </Row>
       </div>
     );
   }
